7bdb1db75e422a6ce71847b7d7c5dcd15e2a1aff
shravan-saini <shravansaini94@gmail.com>
2017-05-24 13:29:10 +0530

    Add files via upload
    Programs for package

---
+++ packageDemo.java
@@ -0,0 +1,9 @@
+package p1;
+public class packageDemo	{
+	public static void main(String[] args)
+	{
+		Protection p= new Protection();
+		Derived d= new Derived();
+		samePackage s= new samePackage();
+	}
+}
